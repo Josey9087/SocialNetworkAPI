98a0421bd20a7be3cefc2a1bc0dc7091cdc16283
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-14 15:51:29 -0700

    adding get contoller for single thought

--- controllers./thoughtsControllers.js
+++ controllers./thoughtsControllers.js
@@ -6,4 +6,14 @@ module.exports = {
       .then((thoughts) => res.json(thoughts))
       .catch((err) => res.status(500).json(err));
   },
+  getOneThought(req, res) {
+    Thoughts.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "Couldn't find this id!!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  }
 };
