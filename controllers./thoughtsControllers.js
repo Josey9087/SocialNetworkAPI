435d2f70f5a34e7393669149c86954efd3177e9d
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-14 15:49:00 -0700

    adding get contoller

--- controllers./thoughtsControllers.js
+++ controllers./thoughtsControllers.js
@@ -1,1 +1,9 @@
 const { Thoughts, User } = require("../models");
+
+module.exports = {
+  getThoughts(req, res) {
+    Thoughts.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+};
