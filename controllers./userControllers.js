df87d74ee0ef7512c543be7e0a1f4b80fabca00a
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-14 17:00:40 -0700

    adding user get one controller

--- controllers./userControllers.js
+++ controllers./userControllers.js
@@ -6,4 +6,16 @@ module.exports = {
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
+  getOneUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .populate("thoughts")
+      .populate("friends")
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "Couldn't find this id!!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 };
