26bc78cf43b9ce162d11ff71234090d45a8f66dc
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-14 17:03:43 -0700

    adding create user controller

--- controllers./userControllers.js
+++ controllers./userControllers.js
@@ -18,4 +18,9 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
 };
