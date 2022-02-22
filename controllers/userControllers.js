7e5911dc3ef1d11a470a501f0bba04e61307d19f
Josey9087 <88466226+Josey9087@users.noreply.github.com>
2022-02-22 10:11:51 -0700

    adding thought on get all users

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,6 +3,9 @@ const { User, Thoughts } = require("../models");
 module.exports = {
   getUsers(req, res) {
     User.find()
+    .select("-__v")
+    .populate("thoughts")
+    .populate("friends")
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
